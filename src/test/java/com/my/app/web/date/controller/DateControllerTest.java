62c29329a648bc305cdc11e0d6676e6ab2bd319f
kdw <cream-soda-@hanmail.net>
2015-03-26 00:34:06 +0900

    add

--- src/test/java/com/my/app/web/date/controller/DateControllerTest.java
+++ src/test/java/com/my/app/web/date/controller/DateControllerTest.java
@@ -1,5 +1,6 @@
 package com.my.app.web.date.controller;
 
+import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -13,7 +14,11 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpSession;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -37,6 +42,10 @@ public class DateControllerTest {
 	public void setUp() {
 		MockitoAnnotations.initMocks(this);
 		this.mockMvc = MockMvcBuilders.standaloneSetup(this.dateController).build();
+		
+		UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken("test", "test");
+		authentication.setDetails(new WebAuthenticationDetails(mock(MockHttpServletRequest.class)));
+		SecurityContextHolder.getContext().setAuthentication(authentication);
 	}
 
 	@Test
