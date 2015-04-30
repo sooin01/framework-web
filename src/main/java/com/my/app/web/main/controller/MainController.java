148c0c47cb813c7bf4abe17646a09f0b52709ed8
Administrator <Administrator@viki>
2015-04-30 22:10:29 +0900

    add

--- src/main/java/com/my/app/web/main/controller/MainController.java
+++ src/main/java/com/my/app/web/main/controller/MainController.java
@@ -1,5 +1,9 @@
 package com.my.app.web.main.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,8 +11,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MainController {
 
+	Logger logger = LoggerFactory.getLogger(MainController.class);
+	
 	@RequestMapping(value = {"/", "/main"}, method = RequestMethod.GET)
 	public String main() {
+		User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		this.logger.info("User info : {}", user);
 		return "main/main";
 	}
 	
